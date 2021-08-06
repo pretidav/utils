0a48d619fff9aeeb92c68ff4e7393765c7e35b88
d.preti <d.preti@almawave.it>
2021-08-06 18:24:31 +0200

    added semaphore and pool

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -1,9 +1,10 @@
-from threading import Thread, Lock
-from multiprocessing import cpu_count
+from threading import Thread, Lock, Barrier
+from multiprocessing import BoundedSemaphore, cpu_count, Pool
 import time
 from random import randint
 from queue import Queue
-
+from tqdm import tqdm
+import numpy as np
 
 #https://www.html.it/pag/69486/multithreading/
 
@@ -35,6 +36,42 @@ class myThread_sync(Thread):
       threadLock.release()
       print("Thread '" + self.name + "' lock released")
 
+
+barrier = Barrier(3,timeout=10)
+class myThread_barrier(Thread):
+    def __init__(self, nome, durata):
+        Thread.__init__(self)
+        self.nome = nome
+        self.durata = durata
+    def run(self):
+        print ("Thread '" + self.name + "' start")
+        # Acquisizione del lock
+        time.sleep(self.durata)
+        print("Parties = " + str(barrier.parties) + "\n")
+        print("n_waiting = " + str(barrier.n_waiting) + "\n")
+        barrier.wait()
+        print ("Thread '" + self.name + "' end")
+        
+
+bsemaphore = BoundedSemaphore(2)
+class myThread_boundsemaphore(Thread):
+    def __init__(self, nome, durata):
+        Thread.__init__(self)
+        self.nome = nome
+        self.durata = durata
+    def run(self):
+        print ("Thread '" + self.name + "' start")
+        # Acquisizione del lock
+        bsemaphore.acquire()
+        print("Thread '" + self.name + "' semaphore acquired")
+        time.sleep(self.durata)
+        print ("Thread '" + self.name + "' end")
+        # Rilascio del lock
+        bsemaphore.release()
+        print("Thread '" + self.name + "' semaphore released")
+
+        
+
 def start(Th,n):
     # Creazione dei thread
     threads = []
@@ -48,7 +85,6 @@ def start(Th,n):
     # Join
     for th in threads:
         th.join()       #until all these threads are over, the code does not proceed further. 
-
     print("Fine")
 
 
@@ -66,12 +102,21 @@ def myfuncqueue(name,q):
         q.task_done()
     return True
 
+def cube(x):
+    return x**3
+
 if __name__=='__main__':
     N=8
     print('# ASYNC #')
     start(Th=myThread_async,n=N)
     print('# SYNC #')
     start(Th=myThread_sync,n=N)
+    print('# BARRIER #')
+    start(Th=myThread_barrier,n=3)
+    print('# BOUNDED SEMAPHORE #')
+    start(Th=myThread_boundsemaphore,n=5)
+    
+
 
     print('# FUNCTION THREAD #')
     N=2
@@ -101,4 +146,29 @@ if __name__=='__main__':
         process = Thread(target=myfuncqueue, args=[str(i),q]) 
         process.start()
     q.join() #threads are executed until all the queue is empty
+
+
+    print('##### POOL ######')
+
+    print('# POOL apply')
+    pool = Pool(processes=4)
+    results = [pool.apply(cube, args=(x,)) for x in range(1,7)]
+    print(results)
+
+    print('# POOL amap')
+    pool = Pool(processes=4)
+    results = pool.map(cube, range(1,7))
+    print(results)
+
+    print('# POOL apply async')
+    pool = Pool(processes=4)
+    results = [pool.apply_async(cube, args=(x,)) for x in range(1,7)]
+    output = [p.get() for p in results]
+    print(output)
+
+    print('# POOL imap')
+    rr = list(np.arange(1,7,1))
+    with Pool(processes=4) as p:
+        A=list(tqdm(p.imap(cube, rr),total=len(rr)))
+    print(A)
 
